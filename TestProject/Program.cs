113db21064a3889b048d7e55a1c70244d97014d8
Mark <lebblu4el@gmail.com>
2018-08-05 14:00:47 +0500

    hi

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -8,9 +8,17 @@ namespace TestProject
 {
     class Program
     {
+        double Square(double sum)
+        {
+            return Math.Sqrt(Sum(2, 3));
+        }
+        double Sum(int a, int b)
+        {
+            return 0;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloy world!");
+            Console.WriteLine("Hello world!");
         }
     }
 }
