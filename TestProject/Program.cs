a0c965dd858ce87003d49ce23666af7e654e949e
Хорищенко Евгений Константинович <zukaatoku@gmail.com>
2018-08-05 13:56:28 +0500

    ЗАРЕФАКТОРИЛ

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestProject
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void SayHello()
         {
             Console.WriteLine("Helloy world!");
+        }
+        static void Main(string[] args)
+        {
+            SayHello();
         }
     }
 }
